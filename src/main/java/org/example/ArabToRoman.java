fda950cea3dbfc664742507f8424e9dc9eb241bb
Юрий Ярцев <urisda1979@gmail.com>
2023-02-12 15:39:47 +0400

    Отработка замечаний от ментора: 1. Реализован класс Main с методом public static String calc(String input). 2.  Реализовано выбрасывание исключений при вводе более двух операндов, например, 1 + 1 + 1.

--- src/main/java/org/example/ArabToRoman.java
+++ src/main/java/org/example/ArabToRoman.java
@@ -3,6 +3,13 @@ package org.example;
 class ArabToRoman {
 
     public static String convertArabToRoman(int numArabian) {
+        if (numArabian < 0) {
+            try {
+                throw new InputMismatchException("В римской системе нет отрицательных чисел");
+            } catch (InputMismatchException e) {
+                throw new RuntimeException(e);
+            }
+        }
         String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
                 "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
